07c1ac79e9d1c1cb870b5fd73be200e0f80a7821
elhenry <elhenry@bu.edu>
2024-12-06 11:12:14 -0500

    Added database table

--- meal_max/meal_max/db.py
+++ meal_max/meal_max/db.py
@@ -1,3 +1,29 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///calorie_tracker.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
+    return app
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    calorie_goal = db.Column(db.Integer, nullable=False)
+    starting_weight = db.Column(db.Float, nullable=False)  
+    
+class WeightLog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+
+class CalorieIntake(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
