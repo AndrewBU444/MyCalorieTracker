e20b5a9377b2683359c3de4a5a324551b2b04eec
elhenry <elhenry@bu.edu>
2024-12-10 00:17:00 -0500

    Created smoketests and edited app.py/sql_utils

--- meal_max/app.py
+++ meal_max/app.py
@@ -6,6 +6,7 @@ from config import ProductionConfig
 from api_client import CalorieNinjasAPIClient  # Change from 'get_nutrition' to the class
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
+from meal_max.utils.sql_utils import check_database_connection, check_table_exists
 
 # Load environment variables from .env file
 load_dotenv()
@@ -23,6 +24,40 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize the database
 db = SQLAlchemy(app)
+
+#Health Checks
+@app.route('/api/health', methods=['GET'])
+def healthcheck() -> Response:
+    """
+    Health check route to verify the service is running.
+
+    Returns:
+        JSON response indicating the health status of the service.
+    """
+    app.logger.info('Health check')
+    return make_response(jsonify({'status': 'healthy'}), 200)
+
+@app.route('/api/db-check', methods=['GET'])
+def db_check() -> Response:
+    """
+    Route to check if the database connection and meals table are functional.
+
+    Returns:
+        JSON response indicating the database health status.
+    Raises:
+        404 error if there is an issue with the database.
+    """
+    try:
+        app.logger.info("Checking database connection...")
+        check_database_connection()
+        app.logger.info("Database connection is OK.")
+        app.logger.info("Checking if meals table exists...")
+        check_table_exists("meals")
+        app.logger.info("meals table exists.")
+        return make_response(jsonify({'database_status': 'healthy'}), 200)
+    except Exception as e:
+        return make_response(jsonify({'error': str(e)}), 404)
+
 
 # User model with password hashing and salt
 class User(db.Model):
